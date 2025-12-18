54f3f279bb72828c6e0a99cc007b4afdbb0343d2
bala-amypo <bala@amypo.in>
2025-12-18 04:39:06 +0000

    User commit

--- src/main/java/com/example/demo/model/Influencer.java
+++ src/main/java/com/example/demo/model/Influencer.java
@@ -9,4 +9,21 @@ public class Influencer{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String socialHandle;
+
+    private String email;
+
+    private Boolean active =true;
+
+    private LocalDateTime createdAt;
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+
 }
