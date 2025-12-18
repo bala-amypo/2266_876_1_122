36794dbcbe3f91a7c7e3281ee99c1e05fb133abd
bala-amypo <bala@amypo.in>
2025-12-18 06:08:07 +0000

    User commit

--- src/main/java/com/example/demo/model/Campaign.java
+++ src/main/java/com/example/demo/model/Campaign.java
@@ -1,4 +1,25 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import java.
+import java.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+
+public class Campaign{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String campaignName;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    private BigDecimal budget;
+
+
+}
