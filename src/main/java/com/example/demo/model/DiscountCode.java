3cb5388107fb27d263bbedaf126f199d4f243904
bala-amypo <bala@amypo.in>
2025-12-18 06:23:06 +0000

    User commit

--- src/main/java/com/example/demo/model/DiscountCode.java
+++ src/main/java/com/example/demo/model/DiscountCode.java
@@ -18,10 +18,59 @@ public class DiscountCode{
     private Boolean active = true;
 
     @ManyToOne
-  @JoinColumn(name = "influencer_id", nullable = false)
-private Influencer influencer;
+    @JoinColumn(name = "influencer_id", nullable = false)
+    private Influencer influencer;
 
+    @ManyToOne
+    @JoinColumn(name = "campaign_id", nullable = false)
+    private Campaign campaign;
 
+    
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(Integer discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Influencer getInfluencer() {
+        return influencer;
+    }
+
+    public void setInfluencer(Influencer influencer) {
+        this.influencer = influencer;
+    }
+
+    public Campaign getCampaign() {
+        return campaign;
+    }
+
+    public void setCampaign(Campaign campaign) {
+        this.campaign = campaign;
+    }
 }
