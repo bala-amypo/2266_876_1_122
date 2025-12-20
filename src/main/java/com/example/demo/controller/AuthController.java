fb87131393338e8c8c98542c8e6b496e8e230bd7
bala-amypo <bala@amypo.in>
2025-12-20 15:58:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    // Empty controller
+    // Authentication will be implemented in later phase
+}
